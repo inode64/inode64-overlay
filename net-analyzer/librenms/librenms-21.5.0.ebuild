911cafbafda219bcb36e1d460041f4216e8ec676
INODE64 <ffelix@inode64.com>
2021-07-02 15:01:09 +0200

    Update headers, missing metadata and I create a script to update all packages

--- net-analyzer/librenms/librenms-21.5.0.ebuild
+++ net-analyzer/librenms/librenms-21.5.0.ebuild
@@ -11,7 +11,7 @@ HOMEPAGE="https://www.librenms.org"
 
 if [[ "${PV}" != 9999 ]] ; then
 	SRC_URI="https://github.com/${PN}/${PN}/archive/${PV}.tar.gz -> ${P}.tar.gz"
-	KEYWORDS="amd64"
+	KEYWORDS="~amd64"
 else
 	inherit git-r3
 	EGIT_REPO_URI="https://github.com/${PN}/${PN}"
---
+++ update_ebuild.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+find . -name "*.ebuild" | while read -r ebuild; do
+  sed -i '/^# Copyright/d' "${ebuild}"
+  sed -i '/^# Distributed/d' "${ebuild}"
+
+  sed -i "1s/^/# Distributed under the terms of the GNU General Public License v2\n/" "${ebuild}"
+  sed -i "1s/^/# Copyright 1999-$(date +%Y) Gentoo Authors\n/" "${ebuild}"
+
+  metadata=$(dirname "${ebuild}")/metadata.xml
+
+  if [ ! -e "${metadata}" ]; then
+    tee "${metadata}" <<EOF >/dev/null
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE pkgmetadata SYSTEM "http://www.gentoo.org/dtd/metadata.dtd">
+<pkgmetadata>
+<maintainer type="person">
+    <email>ffelix@inode64.com</email>
+    <name>Fco. Javier Félix</name>
+</maintainer>
+<maintainer type="project">
+    <email>proxy-maint@gentoo.org</email>
+    <name>Proxy Maintainers</name>
+</maintainer>
+</pkgmetadata>
+EOF
+  fi
+  ebuild "${ebuild}" digest 2>/dev/null
+done
+
+repoman 2>/dev/null
