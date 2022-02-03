08361b2f28da59ee90ee210826bbc364821d9910
Francisco Javier <web@inode64.com>
2022-02-03 06:46:14 +0100

    Reformat code
    Signed-off-by: Fco Javier Felix <ffelix@inode64.com>

--- update_ebuild.sh
+++ update_ebuild.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 find . -name "*.ebuild" | while read -r ebuild; do
-  sed -i '/^# Copyright/d' "${ebuild}"
-  sed -i '/^# Distributed/d' "${ebuild}"
+	sed -i '/^# Copyright/d' "${ebuild}"
+	sed -i '/^# Distributed/d' "${ebuild}"
 
-  sed -i "1s/^/# Distributed under the terms of the GNU General Public License v2\n/" "${ebuild}"
-  sed -i "1s/^/# Copyright 1999-$(date +%Y) Gentoo Authors\n/" "${ebuild}"
+	sed -i "1s/^/# Distributed under the terms of the GNU General Public License v2\n/" "${ebuild}"
+	sed -i "1s/^/# Copyright 1999-$(date +%Y) Gentoo Authors\n/" "${ebuild}"
 
-  metadata=$(dirname "${ebuild}")/metadata.xml
+	metadata=$(dirname "${ebuild}")/metadata.xml
 
-  if [ ! -e "${metadata}" ]; then
-    tee "${metadata}" <<EOF >/dev/null
+	if [ ! -e "${metadata}" ]; then
+		tee "${metadata}" <<EOF >/dev/null
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
 <pkgmetadata>
@@ -24,8 +24,8 @@ find . -name "*.ebuild" | while read -r ebuild; do
     </maintainer>
 </pkgmetadata>
 EOF
-  fi
-  ebuild "${ebuild}" digest 2>/dev/null
+	fi
+	ebuild "${ebuild}" digest 2>/dev/null
 done
 
 repoman 2>/dev/null
