7acdf00b269ffbb217b59f0ca0912d5f368eb256
Fco Javier Felix <ffelix@inode64.com>
2025-01-02 07:42:16 +0100

    updaate hooks post

--- scripts/hooks/post/dist.sh
+++ scripts/hooks/post/dist.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-cmd=$1
-search_dir=$2
-cp=$3
-old_version=$4
-new_version=$5
-new_sha=${6:-}
-old_sha=${7:-}
-hash_date=${8:-}
+search_dir=$1
+cp=$2
+old_version=$3
+new_version=$4
+new_sha=${5:-}
+old_sha=${6:-}
+hash_date=${7:-}
 
 ebuild="$(echo "$cp"|cut -d/ -f2)"
 distdir="$(portageq envvar DISTDIR 2>/dev/null)"
 
-if [ "$cmd" == "post" ]; then
-	if [ "$cp" == "app-misc/node-red" ]; then
-		file="${ebuild}-${new_version}-node_modules.tar.xz"
-		cp ${distdir}/${file} ${search_dir}/dist/
-		git add ${search_dir}/dist/${file}
-		git commit -m "Add distfile for ${cp}"
-	fi
+if [ "$cp" == "app-misc/node-red" ] || [ "$cp" == "dev-lang/typescript" ]; then
+	file="${ebuild}-${new_version}-node_modules.tar.xz"
+	cp ${distdir}/${file} ${search_dir}/dist/
+	git add ${search_dir}/dist/${file}
+	git commit -m "Add distfile for ${cp}"
+fi
 
-	if [ "$cp" == "net-vpn/cloudflared" ]; then
-		file="${ebuild}-${new_version}-vendor.tar.xz"
-		cp ${distdir}/${file} ${search_dir}/dist/
-		git add ${search_dir}/dist/${file}
-		git commit -m "Add distfile for ${cp}"
-	fi
+if [ "$cp" == "net-vpn/cloudflared" ]; then
+	file="${ebuild}-${new_version}-vendor.tar.xz"
+	cp ${distdir}/${file} ${search_dir}/dist/
+	git add ${search_dir}/dist/${file}
+	git commit -m "Add distfile for ${cp}"
 fi
 
 exit 0
