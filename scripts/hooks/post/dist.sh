9f8dcbb3d68af4b1f0c4dffcdfcd27ffff37bda7
Fco Javier Felix <ffelix@inode64.com>
2025-01-04 09:34:24 +0100

    Include support for media-video/go2rtc and app-misc/zigbee2mqtt in script

--- scripts/hooks/post/dist.sh
+++ scripts/hooks/post/dist.sh
@@ -11,14 +11,17 @@ hash_date=${7:-}
 ebuild="$(echo "$cp"|cut -d/ -f2)"
 distdir="$(portageq envvar DISTDIR 2>/dev/null)"
 
-if [ "$cp" == "app-misc/node-red" ] || [ "$cp" == "dev-lang/typescript" ]; then
+if [ "$cp" == "app-misc/node-red" ] || \
+    [ "$cp" == "dev-lang/typescript" ] || \
+    [ "$cp" == "app-misc/zigbee2mqtt" ] ; then
 	file="${ebuild}-${new_version}-node_modules.tar.xz"
 	cp ${distdir}/${file} ${search_dir}/dist/
 	git add ${search_dir}/dist/${file}
 	git commit -m "Add distfile for ${cp}"
 fi
 
-if [ "$cp" == "net-vpn/cloudflared" ]; then
+if [ "$cp" == "net-vpn/cloudflared" ] || \
+    [ "$cp" == "media-video/go2rtc" ] ; then
 	file="${ebuild}-${new_version}-vendor.tar.xz"
 	cp ${distdir}/${file} ${search_dir}/dist/
 	git add ${search_dir}/dist/${file}
